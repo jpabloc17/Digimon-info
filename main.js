e82f710d95ab690c89214193163445e7586cf834
Pablo Castro <pablocas-tro1@outlook.com>
2023-07-02 19:41:19 -0400

    fetch digimon data from an API

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("hi");
+fetch("https://digimon-api.com/api/v1/digimon?pageSize=20")
+  .then((response) => response.json())
+  .then((data) => createDigimonCard(data));
+
+function createDigimonCard(data) {
+  console.log(data);
+}
