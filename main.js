63fb7e66892d5f278d346ac0510212e3568b8c88
Pablo Castro <pablocas-tro1@outlook.com>
2023-07-02 19:54:29 -0400

    create a div element for each digimon

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const cardSection = document.querySelector("#digimons-container");
+console.log(cardSection);
 fetch("https://digimon-api.com/api/v1/digimon?pageSize=20")
   .then((response) => response.json())
-  .then((data) => createDigimonCard(data));
+  .then((digimonData) => createDigimonCard(digimonData.content));
 
-function createDigimonCard(data) {
-  console.log(data);
+function createDigimonCard(digimons) {
+  digimons.forEach((element) => {
+    const div = document.createElement("div");
+    div.classList.add("digimon-card");
+    cardSection.appendChild(div);
+  });
 }
