32483f768aea34024749742cdaad58c1e385b736
Pablo Castro <pablocas-tro1@outlook.com>
2023-07-02 20:20:38 -0400

    create id, name &  img for each digimon

--- main.js
+++ main.js
@@ -5,9 +5,17 @@ fetch("https://digimon-api.com/api/v1/digimon?pageSize=20")
   .then((digimonData) => createDigimonCard(digimonData.content));
 
 function createDigimonCard(digimons) {
-  digimons.forEach((element) => {
+  digimons.forEach((digimon) => {
     const div = document.createElement("div");
     div.classList.add("digimon-card");
+    const digimonId = document.createElement("span");
+    digimonId.textContent = `#${digimon.id}`;
+    const name = document.createElement("h3");
+    name.textContent = digimon.name;
+    const img = document.createElement("img");
+    img.src = digimon.image;
+    img.alt = `${digimon.name} Picture`;
+    console.log(digimonId, name, img);
     cardSection.appendChild(div);
   });
 }
